b684ac8587263662ecdcadc61076851302dfc99b
KingKyorii <azusibabu@gmail.com>
2025-07-01 16:06:01 +0700

    coba

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'profil/profil_screen.dart';
+import 'user_login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: ProfilScreen(),
+      home: LoginScreen(),
     );
   }
 }
